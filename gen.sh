6820a25d9c46c1bd8ffec5cf1a09c2be56f397f9
kaotisk <kaotisk@arching-kaos.com>
2021-03-15 01:12:43 +0200

    Ready to run for first time [no backup]

--- gen.sh
+++ gen.sh
@@ -8,5 +8,11 @@
 #cjdroute --genconf | sed -n '/^\/\*.*\*\//!p' | sed -n '/ \/\/.*/!p' | sed 's|/\*|\n&|g;s|*/|&\n|g' | sed '/\/\*/,/*\//d' > cjdroute.conf
 
 # Futuristic way B-)
-# cjdroute --genconf | cjdroute --cleanconf > cjdroute.conf
+sudo dnf install cjdns-tools -y
+sudo systemctl start cjdns
+sudo systemctl stop cjdns
+cjdroute --genconf | cjdroute --cleanconf > cjdroute.conf
 python appendPeers.py
+sudo cat cjdroute.conf > /etc/cjdroute.conf
+sudo systemctl enable cjdns
+sudo systemctl start cjdns
