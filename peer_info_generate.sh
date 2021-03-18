f057c1b520ff44b9bceb9827f7a7856f0d069b8e
kaotisk <kaotisk@arching-kaos.com>
2021-03-19 01:14:20 +0200

    Added shell script for help running and applying the new credentials

---
+++ peer_info_generate.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# We run this like this !!!
+echo "Exporting configuration to workfile..."
+cat /etc/cjdroute.conf | cjdroute --cleanconf > cjdroute.conf
+echo "Backup existing configuration..."
+cp /etc/cjdroute.conf /etc/cjdroute.conf-backup
+echo "Starting script..."
+python peer_info_generate.sh
+echo "Assuming everything went well ^.^"
+echo "Installing new configuration..."
+cat cjdroute.conf > /etc/cjdroute.conf
+echo "Restarting CJDNS with new peer slots..."
+systemctl restart cjdns.service
