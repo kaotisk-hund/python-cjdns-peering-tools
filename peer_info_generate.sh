7b86fc3bbe6ae7b7cbdc4c97edfabd5365b465bc
kaotisk <kaotisk@arching-kaos.com>
2021-03-20 06:01:26 +0200

    Found it

--- peer_info_generate.sh
+++ peer_info_generate.sh
@@ -5,7 +5,7 @@ cat /etc/cjdroute.conf | cjdroute --cleanconf > cjdroute.conf
 echo "Backup existing configuration..."
 cp /etc/cjdroute.conf /etc/cjdroute.conf-backup
 echo "Starting script..."
-python peer_info_generate.sh
+python peer_info_generate.py
 echo "Assuming everything went well ^.^"
 echo "Installing new configuration..."
 cat cjdroute.conf > /etc/cjdroute.conf
